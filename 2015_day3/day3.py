d0658f0074b052a3be0203a44c89dc1cae99b57e
mbagic <matija.bagic@reversinglabs.com>
2016-11-16 10:41:21 +0100

    day3 2015 full solution

--- 2015_day3/day3.py
+++ 2015_day3/day3.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python
 import sys
 
-OPEN_BRACKET = '('
-CLOSED_BRACKET = ')'
+NORTH = '^'
+EAST = '>'
+SOUTH = 'v'
+WEST = '<'
 
-def calculate(brackets):
-    floor = 0
-    for bracket in brackets:
-        if bracket == OPEN_BRACKET:
-            floor += 1
-        elif bracket == CLOSED_BRACKET:
-            floor -= 1
 
-    return floor
+def move(location, instruction):
+    if instruction == NORTH:
+        return location[0], location[1]+1
+    if instruction == SOUTH:
+        return location[0], location[1]-1
+    if instruction == EAST:
+        return location[0]+1, location[1]
+    if instruction == WEST:
+        return location[0]-1, location[1]
 
 if __name__ == '__main__':
+    location = (0, 0)
+    already_visited = set()
+    already_visited.add(location)
+
+    gift_count = 1
+
     with open(sys.argv[1], 'r') as f:
-        brackets = f.read().strip("\n")
-        print brackets
-        print calculate(brackets)
+        instructions = f.read().strip("\n")
+        for instruction in instructions:
+            location = move(location, instruction)
+            if location not in already_visited:
+                gift_count += 1
+                already_visited.add(location)
+
+    print gift_count
+
+
+
